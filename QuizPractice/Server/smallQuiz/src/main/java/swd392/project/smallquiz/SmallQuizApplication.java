1e68f7e13e5bff51f3b76a6f87374a82213af27f
namnthe150933 <namthanhnguyen601@gmail.com>
2023-01-30 16:46:37 +0700

    addspring boot project

---
+++ QuizPractice/Server/smallQuiz/src/main/java/swd392/project/smallquiz/SmallQuizApplication.java
@@ -0,0 +1,13 @@
+package swd392.project.smallquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallQuizApplication.class, args);
+	}
+
+}
---
+++ QuizPractice/Server/smallQuiz/src/test/java/swd392/project/smallquiz/SmallQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package swd392.project.smallquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
