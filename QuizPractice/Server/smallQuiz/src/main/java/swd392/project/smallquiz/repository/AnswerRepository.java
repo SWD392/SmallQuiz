92ddf6cdf1b3ade88468059155d4fd6347976cd1
phamsangvd <105622184+phamsangvd@users.noreply.github.com>
2023-02-15 21:30:40 +0700

    fix AnswerRepository

--- QuizPractice/Server/smallQuiz/src/main/java/swd392/project/smallquiz/repository/AnswerRepository.java
+++ QuizPractice/Server/smallQuiz/src/main/java/swd392/project/smallquiz/repository/AnswerRepository.java
@@ -2,7 +2,6 @@ package swd392.project.smallquiz.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import swd392.project.smallquiz.model.dto.AnswerDto;
 import swd392.project.smallquiz.model.entiity.Answer;
 import swd392.project.smallquiz.model.entiity.Question;
 
@@ -11,6 +10,6 @@ import java.util.List;
 @Repository
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
 
-    List<AnswerDto> findByQuestion(Question question);
+    List<Answer> findByQuestion(Question question);
 
 }
