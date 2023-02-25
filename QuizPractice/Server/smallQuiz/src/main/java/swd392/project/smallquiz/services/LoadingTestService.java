44883530ce3ff53f8fb2d68b2417d59e32675d5c
namnthe150933 <namthanhnguyen601@gmail.com>
2023-02-25 14:10:44 +0700

    refactor api loadtest

--- QuizPractice/Server/smallQuiz/src/main/java/swd392/project/smallquiz/services/LoadingTestService.java
+++ QuizPractice/Server/smallQuiz/src/main/java/swd392/project/smallquiz/services/LoadingTestService.java
@@ -1,5 +1,6 @@
 package swd392.project.smallquiz.services;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import swd392.project.smallquiz.mapper.AnswerMapper;
@@ -8,7 +9,7 @@ import swd392.project.smallquiz.model.entiity.Answer;
 import swd392.project.smallquiz.model.entiity.Question;
 import swd392.project.smallquiz.repository.AnswerRepository;
 import swd392.project.smallquiz.repository.QuestionRepository;
-import swd392.project.smallquiz.response.TestResponse;
+import swd392.project.smallquiz.response.QuestionResponse;
 
 import java.util.*;
 
@@ -20,31 +21,34 @@ public class LoadingTestService {
     QuestionRepository questionRepository;
     @Autowired
     AnswerRepository answerRepository;
-    public List<TestResponse> loadTest(){
+    public List<QuestionResponse> loadTest(){
         List<Question> questionList= questionRepository.findAllByDeleteFlag(false);
         Map<Integer,Question> map= new HashMap<>();
         for(int i=0;i<questionList.size();i++){
             map.put(i,questionList.get(i));
         }
-        List<TestResponse> testResponses= new ArrayList<>();
+        List<QuestionResponse> questionResponseList= new ArrayList<>();
         if(questionList.size()>10) {
             for (int i = 0; i < 10; i++) {
                 Random rd = new Random();
                 int number = rd.nextInt(questionList.size());
-                addListTest(map, testResponses, number);
+                addListTest(map, questionResponseList, number);
             }
         }
         else{
             for (int i = 0; i < questionList.size(); i++) {
-                addListTest(map, testResponses, i);
+                addListTest(map, questionResponseList, i);
             }
         }
-        return testResponses;
+        return questionResponseList;
     }
 
-    private void addListTest(Map<Integer, Question> map, List<TestResponse> testResponses, int index) {
+    private void addListTest(Map<Integer, Question> map, List<QuestionResponse> questionResponseList, int index) {
         List<Answer> answerList = answerRepository.findByQuestion(map.get(index));
         List<AnswerDto> answerDtoList= answerMapper.convertAnswer(answerList);
-        testResponses.add(new TestResponse(map.get(index), answerDtoList));
+        QuestionResponse questionResponse= new QuestionResponse();
+        questionResponse.setAnswers(answerDtoList);
+        BeanUtils.copyProperties(map.get(index), questionResponse);
+        questionResponseList.add(questionResponse);
     }
 }
