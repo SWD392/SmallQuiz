0eab294b8e8c0b304baaa5e6a64895424e7341ff
thinhnqhe150498 <105189247+thinhnqhe150498@users.noreply.github.com>
2023-03-03 16:29:44 +0700

    Add Question

--- QuizPractice/Client/small-quiz/src/pages/List_question.js
+++ QuizPractice/Client/small-quiz/src/pages/List_question.js
@@ -1,16 +1,62 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axiosInstance from "../api/axiosInstance";
+import { Navbar } from "./Navbar";
 export default function List_question() {
   const [info, setInfo] = useState([]);
   const token = localStorage.getItem("token");
   const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [ispopupCreateopen, setopopupCreate] = useState(false);
   const [selectedQuestion, setSelectedQuestion] = useState(null);
-  
+  const [answer1, setAnswer1] = useState("");
+  const [answer, setAnswer] = useState();
+  const [answer2, setAnswer2] = useState("");
+  const [answer3, setAnswer3] = useState("");
+  const [answer4, setAnswer4] = useState("");
+  const [questions, setQuestions] = useState("");
+  const [index, setIndex] = useState(0);
   const handleEdit = (question) => {
     setSelectedQuestion(question);
     setIsPopupOpen(true);
   };
+  const handleCreate = () => {
+    setopopupCreate(true);
+  }
+
+  const handleSubmitCreate = async (event) => {
+    const dataCreate = {
+      questionContent: questions,
+      answers: [
+        {
+          content: answer1,
+          status: answer === answer1 ? true : false
+        },
+        {
+          content: answer2,
+          status: answer === answer2 ? true : false
+        },
+        {
+          content: answer3,
+          status: answer === answer3 ? true : false
+        },
+        {
+          content: answer4,
+          status: answer === answer4 ? true : false
+        },
+      ]
+    }
+
+
+    try {
+      const res = await axiosInstance.post(
+        `/admin/create_question`,
+        dataCreate
+      );
+    } catch (error) {
+      console.error(error);
+    }
+
+  }
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -25,33 +71,30 @@ export default function List_question() {
       })),
     };
 
+
+
     try {
       const res = await axiosInstance.put(
         `/admin/update_question?questionId=${questionId}`,
         data
       );
-      
       setInfo((prevInfo) =>
         prevInfo.map((question) => {
           if (question.id === questionId) {
-            return { ...question, content: questionContent, answers: selectedQuestion.answers.map((item) => ({
-              content: item.content,
-              status: item.status,
-            }))};
+            return {
+              ...question, content: questionContent, answers: selectedQuestion.answers.map((item) => ({
+                content: item.content,
+                status: item.status,
+              }))
+            };
           } else {
             return question;
           }
         })
       );
-      
-      
-      
     } catch (error) {
       console.error(error);
     }
-
-    // console.log(selectedQuestion.answers);
-    // console.log(selectedQuestion);
   };
 
   useEffect(() => {
@@ -67,36 +110,19 @@ export default function List_question() {
     };
     fetchData();
   }, [token]);
-  // const decode = token ? jwt_decode(token) : null;
-  // useEffect(() => {
-  //   const fetchInfo = async () => {
-  //     const data = {
-  //       username: decode.sub,
-  //     }
-  //     try {
-  //       axios.post("http://localhost:8081/info",data).then((response) => {
-  //         console.log(response.data);
-  //       });
-  //     } catch (error) {}
-  //   };
-
-  //   fetchInfo();
-  // }, []);
-
-  console.log(selectedQuestion);
   const nagative = useNavigate();
-
   const handleLogout = () => {
     localStorage.removeItem("token");
     localStorage.removeItem("admin");
     nagative("/list_question");
   };
 
+  console.log(answer);
   return (
     <>
+      <Navbar></Navbar>
       <div className="main-content">
         <div className="container mt-7">
-          <h2 className="mb-5">Question List</h2>
           <div className="row">
             <div className="col">
               <div className="card shadow">
@@ -138,10 +164,145 @@ export default function List_question() {
                   </table>
                 </div>
               </div>
+              <button
+                type="button"
+                className="btn btn-success btn"
+                data-toggle="modal"
+                data-target="#contact-modal"
+                onClick={() => handleCreate()}
+              >
+                Create new Question
+              </button>
             </div>
           </div>
         </div>
       </div>
+      {
+        ispopupCreateopen && (
+          <div>
+            <div id="contact-modal" className="modal" role="dialog">
+              <div className="modal-dialog">
+                <div className="modal-content">
+                  <form
+                    id="contactForm"
+                    name="contact"
+                    role="form"
+                    onSubmit={handleSubmitCreate}
+                  >
+                    <div className="modal-header">
+                      <input
+                        type="text"
+                        name="content"
+                        className="form-control"
+
+                        onChange={(e) =>
+                          setQuestions(e.target.value)
+                        }
+                      />
+                      <button
+                        type="button"
+                        className="close"
+                        data-dismiss="modal"
+                      >
+                        &times;
+                      </button>
+                    </div>
+                    <div className="modal-body">
+                      <div className="form-group">
+                        <label>
+                          Answer 1
+                        </label>
+                        <input
+                          type="text"
+                          name=""
+                          className="form-control"
+                          onChange={(e) =>
+                            setAnswer1(e.target.value)
+                          }
+                        />
+                      </div>
+                      <div className="form-group">
+                        <label>
+                          Answer 2
+                        </label>
+                        <input
+                          type="text"
+                          name=""
+                          className="form-control"
+                          onChange={(e) =>
+                            setAnswer2(e.target.value)
+                          }
+                        />
+                      </div>
+                      <div className="form-group">
+                        <label>
+                          Answer 3
+                        </label>
+                        <input
+                          type="text"
+                          name=""
+                          className="form-control"
+                          onChange={(e) =>
+                            setAnswer3(e.target.value)
+                          }
+                        />
+                      </div>
+                      <div className="form-group">
+                        <label>
+                          Answer 4
+                        </label>
+                        <input
+                          type="text"
+                          name=""
+                          className="form-control"
+                          onChange={(e) =>
+                            setAnswer4(e.target.value)
+                          }
+                        />
+                      </div>
+
+                      <div className="form-group">
+                        <label htmlFor="correct-answer">
+                          Choose a correct answer:
+                        </label>
+                        <select
+                          name="correct-answer"
+                          id="correct-answer"
+                          value={answer}
+                          onChange={((e) => setAnswer(e.target.value))}
+                          className="form-control"
+                        >
+                          <option value={answer1}>Answer 1</option>
+                          <option value={answer2}>Answer 2</option>
+                          <option value={answer3}>Answer 3</option>
+                          <option value={answer4}>Answer 4</option>
+                        </select>
+                      </div>
+                    </div>
+                    <div className="modal-footer">
+                      <button
+                        type="button"
+                        className="btn btn-default"
+                        data-dismiss="modal"
+                      >
+                        Close
+                      </button>
+                      <button
+                        type="submit"
+                        className="btn btn-success"
+                        data-toggle="modal"
+                        data-target="#contact-modal"
+                      >
+                        Save Changes
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        )
+      }
       {isPopupOpen && (
         <div>
           <div id="contact-modal" className="modal" role="dialog">
@@ -205,22 +366,11 @@ export default function List_question() {
                       <select
                         name="correct-answer"
                         id="correct-answer"
+                        value={selectedQuestion.answers[index].content}
                         className="form-control"
-                        
-                        // onChange={(e) =>
-                        //   setSelectedQuestion({
-                        //     ...selectedQuestion,
-                        //     answers: {
-                        //       ...selectedQuestion.answers,
-                        //       status: selectedQuestion.answers.status.map((item) => {
-                        //         return e.target.value;
-                        //       })
-                        //     }
-                        //   })
-                        // }
                       >
                         {selectedQuestion.answers.map((answer, index) => (
-                          <option value={answer.status} key={index}>{answer.content}</option>
+                          <option setIndex={index} value={answer.status} key={index}>{answer.content}</option>
                         ))}
                       </select>
                     </div>
@@ -248,9 +398,9 @@ export default function List_question() {
           </div>
         </div>
 
-
       )}
-      
+
     </>
   );
 }
+
