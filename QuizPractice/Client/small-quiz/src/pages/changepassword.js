51d60dc118ca9b348b56e101800c4b860f880b14
thinhnqhe150498 <105189247+thinhnqhe150498@users.noreply.github.com>
2023-03-16 21:36:23 +0700

    form change password doi api

---
+++ QuizPractice/Client/small-quiz/src/pages/changepassword.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const ChangePassword = () => {
+  const [currentPassword, setCurrentPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (newPassword !== confirmPassword) {
+      setMessage("New password and confirm password do not match");
+      return;
+    }
+
+    axios
+      .post("chuacallapi", {
+        currentPassword: currentPassword,
+        newPassword: newPassword,
+      })
+      .then((response) => {
+        setMessage(response.data.message);
+        setCurrentPassword("");
+        setNewPassword("");
+        setConfirmPassword("");
+      })
+      .catch((error) => {
+        setMessage(error.response.data.message);
+      });
+  };
+
+  return (
+    <div>
+      <h2>Change Password</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="current-password">Current Password:</label>
+          <input
+            type="password"
+            id="current-password"
+            value={currentPassword}
+            onChange={(event) => setCurrentPassword(event.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="new-password">New Password:</label>
+          <input
+            type="password"
+            id="new-password"
+            value={newPassword}
+            onChange={(event) => setNewPassword(event.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="confirm-password">Confirm Password:</label>
+          <input
+            type="password"
+            id="confirm-password"
+            value={confirmPassword}
+            onChange={(event) => setConfirmPassword(event.target.value)}
+          />
+        </div>
+        <button type="submit">Change Password</button>
+      </form>
+      {message && <p>{message}</p>}
+    </div>
+  );
+};
+
+export default ChangePassword;
