4795a871b7f8a2f0efa5d33d392b6ffea4ba5dea
namnthe150933 <namthanhnguyen601@gmail.com>
2023-03-19 16:10:29 +0700

    refactor api info

--- QuizPractice/Server/smallQuiz/src/main/java/swd392/project/smallquiz/request/UserInfoRequest.java
+++ QuizPractice/Server/smallQuiz/src/main/java/swd392/project/smallquiz/request/UserInfoRequest.java
@@ -1,10 +1,12 @@
 package swd392.project.smallquiz.request;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserInfoRequest {
     @NotNull
     private String username;
